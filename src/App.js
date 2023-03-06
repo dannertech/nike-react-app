89562176c114680027474651a4d1d810a6ce616e
dannertech <danner@danner.tech>
2023-03-06 14:24:08 -0500

    a lot of styling changes for the love of God, Mary, and Joseph

--- src/App.js
+++ src/App.js
@@ -3,8 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+      <div className='Image-Container'>
       <div className='Image'>
-        <img src={require('../src/images/pngwing.com.png')} id="Lebron-Image"/>
+        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+      </div>
+      <div className='Image' >
+        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+      </div>
+      <div className='Image' >
+        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+      </div>
+      </div>
+      <div className='Image-Container'>
+      <div className='Image'>
+        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+      </div>
+      <div className='Image' >
+        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+      </div>
+      <div className='Image' >
+        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+      </div>
       </div>
      <div className='Enter-Button-Container'>
         <div className="Enter-Button">
