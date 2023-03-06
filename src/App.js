f29b31de20b143a8ddfd76f0559c47fb134fa3e2
dannertech <danner@danner.tech>
2023-03-06 14:43:37 -0500

    styled main page

--- src/App.js
+++ src/App.js
@@ -5,24 +5,25 @@ function App() {
     <div className='App'>
       <div className='Image-Container'>
       <div className='Image'>
-        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+        <img src={require('../src/images/pngegg.png')} style={{height: '60%', width: '100%'}}/>
       </div>
       <div className='Image' >
-        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+        <img src={require('../src/images/pngegg-2.png')} style={{height: '50%', width: '60%'}}/>
       </div>
       <div className='Image' >
-        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+        <img src={require('../src/images/pngegg-3.png')} style={{height: '100%', width: '100%'}}/>
       </div>
       </div>
       <div className='Image-Container'>
       <div className='Image'>
-        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+        <img src={require('../src/images/pngegg-4.png')} style={{height: '70%', width: '80%'}}/>
       </div>
       <div className='Image' >
-        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+        <img src={require('../src/images/pngegg-5.png')} style={{height: '60%', width: '60%', 
+      }}/>
       </div>
       <div className='Image' >
-        <img src={require('../src/images/pngwing.com.png')} style={{height: '100%', width: '100%'}}/>
+        <img src={require('../src/images/pngegg-7.png')} style={{height: '80%', width: '100%'}}/>
       </div>
       </div>
      <div className='Enter-Button-Container'>
