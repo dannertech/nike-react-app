a6f9f583fa4d55bb93c8b5f73c56cc76802fefb5
dannertech <danner@danner.tech>
2023-03-07 16:14:42 -0500

    created api directory and data file with sam[ple data and pictures

---
+++ src/api/data.js
@@ -0,0 +1,33 @@
+const inventory = [
+    {
+        title: "Nike x Supreme",
+        cost: 500,
+        image: require('../images/nike-supreme.png')
+
+    },
+    {
+        title: "Nike x Off White",
+        cost: 500,
+        image: require('../images/nike-offwhite-dunk.png')
+
+    },
+    {
+        title: "Nike Air Force",
+        cost: 100,
+        image: require('../images/nike-air-force.png')
+
+    },
+    {
+        title: "Nike Infrared",
+        cost: 125,
+        image: require('../images/nike-red.png')
+
+    },
+    {
+        title: "Nike x Supreme Tee",
+        cost: 90,
+        image: require('../images/nike-supreme-tee.png')
+
+    },
+
+]
