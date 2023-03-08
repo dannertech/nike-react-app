6801f9915173dd11c1031ba668f124b9fa33250c
dannertech <danner@danner.tech>
2023-03-08 17:33:00 -0500

    application navigates to Total page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import '../App.css';
+import styled from 'styled-components';
+import {useNavigate} from 'react-router-dom';
+
+
+//styled components
+const Button = styled.button`
+background-color: transparent;
+color: white;
+font-weight: bold;
+font-size: 30px;
+border-style: none;
+`
 
 const Header = () => {
+    const navigate = useNavigate();
     return(
         <div id="Header">
-            <p>This is the header</p>
+            <img src={require('../images/nike-white-logo.png')} style={{height: '50%', width: '10%', marginTop: '3%', marginLeft: '3%'}}/>
+            <Button onClick={() => navigate('/total')} id="Header-Button">Go To Total</Button>
         </div>
     )
 };
