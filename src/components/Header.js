ee6f13490a9dddbf0cf68e54127a2ac427ec64fe
dannertech <danner@danner.tech>
2023-03-08 01:29:29 -0500

    added header to inventory page

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Header = () => {
+    return(
+        <div id="Header">
+            <p>This is the header</p>
+        </div>
+    )
+};
+
+export default Header;
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -2,10 +2,12 @@ import '../App.css'
 import React from 'react';
 import ItemCard from '../components/ItemCard';
 import inventory from '../api/data';
+import Header from '../components/Header';
 
 const Main = () => {
     return (
-        <div style={{backgroundColor: 'white'}}>
+        <div style={{backgroundColor: 'white', overflow: 'auto', marginTop: -16}}>
+            <Header />
             <div id="ItemCardsContainer">
             {inventory.map((item) => {
                 return <ItemCard image={item.image} title={item.title} cost={item.cost}/>
