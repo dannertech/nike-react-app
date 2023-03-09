c6df951b8d383fdce766dc4be82470b0f525a180
dannertech <danner@danner.tech>
2023-03-08 22:10:28 -0500

    added Add To Cart button to ItemCard component

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import '../App.css';
+import styled from 'styled-components';
+
+//styled components
+const Button = styled.button`
+color: black;
+font-weight: bold;
+background-color: transparent;
+border-style: none;
+`
 
 const ItemCard = ({image, cost, title}) => {
     return(
@@ -8,6 +17,9 @@ const ItemCard = ({image, cost, title}) => {
                 <h1 style={{color: 'white'}}>{title}</h1>
                 <img src={image} id="ItemCardImage" />
                 <h3>{`$${cost}`}</h3>
+                <div style={{width: '100%', backgroundColor: 'white', height: '10%'}}>
+                    <Button>Add To Cart</Button>
+                </div>
             </div>
         </div>
     )
