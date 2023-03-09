1547bbb0a45d34746ebd09a36fe8e8c6c5d4deee
dannertech <danner@danner.tech>
2023-03-08 21:56:34 -0500

    styled Total page

--- src/screens/Total.js
+++ src/screens/Total.js
@@ -6,8 +6,10 @@ const Total = () => {
     return(
         <div>
             <Header />
-            <div>
-                <img src={require('../images/nike-mona-lisa.png')}/>
+            <div id='Total-Page-Column'>
+                <img src={require('../images/nike-mona-lisa.png')} style={{height: '20vh', width: '35vw'}}/>
+                <h1 style={{fontWeight: 'bold', fontSize: 70}}>Total</h1>
+                <h1>Total here</h1>
             </div>
         </div>
     )
